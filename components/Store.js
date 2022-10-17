9a99a468cf78dce2974a9dee16cf0b9fead1ffe3
carlos camacho <carlic.dev@gmail.com>
2022-10-17 15:28:30 -0500

    new entry

--- components/Store.js
+++ components/Store.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Store = () => {
     return (
         <div className='container text-white font-semibold text-center'>
-            En construcción. Le falta
+            En construcción. Le falta mucho
         </div>
     )
 }
