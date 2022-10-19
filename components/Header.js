1eac070a266314812b4e817a935d6cb2f75489e2
carlos camacho <carlic.dev@gmail.com>
2022-10-19 15:10:48 -0500

    brand fix

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,7 @@ const Header = () => {
                 <div className='md:float-left block mt-2'>
                     <Link href='/'>
                         <span className='flex cursor-pointer brand font-bold text-5xl text-zinc-500 uppercase'>
+                            {/*}
                             <Image
                                 alt='logo'
                                 height='80px'
@@ -19,6 +20,7 @@ const Header = () => {
                                 className="rounded-full mr-0"
                                 src='/../public/LuisSalImg.png'
                             />
+    {*/}
                             <span className='mt-3'>Luca<span className='text-zinc-300'>Sal</span></span>
                         </span>
                     </Link>
