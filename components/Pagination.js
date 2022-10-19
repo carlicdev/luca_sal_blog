43f0e8f93555d2e62b12823b751c2e5b53add3b0
carlos camacho <carlic.dev@gmail.com>
2022-10-19 17:11:21 -0500

    added pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlineArrowLeft, MdOutlineArrowRight} from 'react-icons/md';
 
 const Pagination = ({totalPosts, postsPerPage, setCurrentPage, currentPage }) => {
   let pages = [];
@@ -8,17 +9,22 @@ const Pagination = ({totalPosts, postsPerPage, setCurrentPage, currentPage }) =>
   }
 
   return (
-    <div className='m-2 p-2'>
+    <div className='m-2 p-2 mx-auto flex'>
+      <button
+        type='button'
+        onClick={() => setCurrentPage(currentPage - 1)}
+      >
+        <MdOutlineArrowLeft size={'4em'} className='text-slate-500 cursor-pointer hover:text-cyan-600'/>
+      </button>
       {
         pages.map((page, index) => {
           return (
-
           <button 
             key={index} 
             className={page == currentPage ? 
-              `rounded text-2xl bg-cyan-600 px-3 py-1 mx-1 text-white` 
+              `rounded text-2xl bg-cyan-600 px-3 py-1 mx-1 my-3 text-white` 
               :
-              `rounded text-xl bg-slate-700 hover:bg-slate-800 px-3 py-1 mx-1 text-white`
+              `rounded text-xl bg-slate-700 hover:bg-slate-800 px-3 py-1 mx-1 my-3 text-white`
             }
             onClick={() => setCurrentPage(page)}
             >
@@ -27,6 +33,12 @@ const Pagination = ({totalPosts, postsPerPage, setCurrentPage, currentPage }) =>
           )
         })
       }
+      <button
+        type='button'
+        onClick={() => setCurrentPage(currentPage + 1)}
+      >
+        <MdOutlineArrowRight size={'4em'} className='text-slate-500 cursor-pointer hover:text-cyan-600'/>
+      </button>
     </div>
   )
 }
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { getCategories, getCategoryPost } from '../../services';
-import { PostCard, Categories, Loader, Subscribe } from '../../components';
+import { PostCard, Categories, Loader, Subscribe, Pagination } from '../../components';
 
 const CategoryPost = ({ posts }) => {
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage; 
+  const currentPosts = posts.slice(firstPostIndex, lastPostIndex)
 
   if (router.isFallback) {
     return <Loader />;
@@ -15,9 +21,15 @@ const CategoryPost = ({ posts }) => {
     <div className="container mx-auto md:px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
+          {currentPosts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
+            <Pagination 
+              totalPosts={posts.length} 
+              postsPerPage={postsPerPage} 
+              setCurrentPage={setCurrentPage} 
+              currentPage={currentPage}
+            />
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
